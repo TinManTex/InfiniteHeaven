07fd73344498d2b373884a1fad934afe4bbb4b65
TinManTex <tinman.squad@gmail.com>
2020-05-30 08:44:59 +1200

    addon: cypr explorer

---
+++ tpp/addons/cypr-explorer/GameDir/mod/missions/12010_cypr.lua
@@ -0,0 +1,12 @@
+local this={
+	description="Cyprus",
+	missionCode=12010,
+    location="CYPR",
+    packs={
+    	"/Assets/tpp/pack/mission2/ih/minimal_mission.fpk",
+    	"/Assets/tpp/pack/location/cypr/pack_common/cypr_script_free.fpk",--NMC usually via TppPackList.AddLocationCommonScriptPack(missionCode)
+   	},
+    startPos={-62.617,90.064,-1565.288},--rotY=-179.287,},
+}
+
+return this
