eaf7fa0463fc1282757d2fae9d8d0aefe7c89075
TinManTex <tinman.squad@gmail.com>
2020-06-02 12:55:00 +1200

    InfInit load external fallback attempt

--- tpp/data1_dat-lua/Assets/tpp/script/ih/InfInit.lua
+++ tpp/data1_dat-lua/Assets/tpp/script/ih/InfInit.lua
@@ -1,14 +1,31 @@
 -- DOBUILD: 1
 -- InfInit.lua
+-- tex loaded in init.lua
+-- is module since it's Script.LoadLibraryed to break init.luas boxing
+-- this does lead to an unknown since (in release build) it will be Script.LoadLibrarying more modules below while the module itself hasn't finished it's loading via Script.LoadLibrary
+-- see also InfInitMain
 local this={}
 
 if IHH then
   IHH.Init()
 end
 
+--OUT/SIDE: InfCore
+--function this.LoadCoreExternal()
+--  InfCore=require"mod/Assets/tpp/script/ih/InfCore"--GOTCHA modPath. relying on default package.paths having game dir
+--end
 --tex try external first
-InfCore=require"mod/Assets/tpp/script/ih/InfCore"--GOTCHA modPath. relying on default package.paths having game dir
-if InfCore==nil then
+--DEBUGNOW a way to output err (if IHH then you can just log it)
+--local ok,err=pcall(this.LoadCoreExternal())--DEBUGNOW pcall not working
+--if err then
+--  Script.LoadLibrary"/Assets/tpp/script/ih/InfCore.lua"
+--end
+
+--tex haven't been able to get a fallback wokeing, so will just hardcode to a bool for now
+local loadExternal=true
+if loadExternal then
+  InfCore=require"mod/Assets/tpp/script/ih/InfCore"--GOTCHA modPath. relying on default package.paths having game dir
+else
   Script.LoadLibrary"/Assets/tpp/script/ih/InfCore.lua"
 end
 
