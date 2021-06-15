4a986b7290a7a19662a3343b48a882ed681502dd
TinManTex <tinman.squad@gmail.com>
2021-06-15 17:42:48 +1200

    Update IvarProc.lua

--- tpp/mod-gamedir/Assets/tpp/script/ih/IvarProc.lua
+++ tpp/mod-gamedir/Assets/tpp/script/ih/IvarProc.lua
@@ -528,7 +528,6 @@ function this.GetVector3(ivar)
   end
   return Vector3(ivar.vec3Ivars.X:Get(),ivar.vec3Ivars.Y:Get(),ivar.vec3Ivars.Z:Get())
 end
-
 function this.SetVector3(ivar,vec3)
   if ivar.vec3Ivars==nil then
     InfCore.Log("ERROR: IvarProc.SetVector3: "..ivar.name..".vec3Ivars==nil")
@@ -538,7 +537,6 @@ function this.SetVector3(ivar,vec3)
   --if is table and .X, else X=vec3[1]
   return {ivar.vec3Ivars.X:Set(vec3[0]),ivar.vec3Ivars.Y:Set(vec3[0]),ivar.vec3Ivars.Z:Set(vec3[0])}
 end
-
 function this.Scale(ivar,value)
   local ivarScale=ivar:Get()
   if ivarScale~=100 then
@@ -547,7 +545,7 @@ function this.Scale(ivar,value)
   else
     return value
   end
-end
+end--Scale
 
 local mbFreeMissions={[30050]=true,[30150]=true,[30250]=true}
 
