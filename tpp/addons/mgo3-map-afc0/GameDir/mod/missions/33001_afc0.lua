e6ce6cae923d629b7ef113edfa6100e0d10f95c4
TinManTex <tinman.squad@gmail.com>
2021-06-23 11:38:29 +1200

    subs_boot_ene_af.fpk

--- tpp/addons/mgo3-map-afc0/GameDir/mod/missions/33001_afc0.lua
+++ tpp/addons/mgo3-map-afc0/GameDir/mod/missions/33001_afc0.lua
@@ -13,7 +13,8 @@ local this={
     TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/ih_soldier_base.fpk"--tex mis_com_mafr isn't actually a complete enemy pack, normal missions roll these files into the mission packs
     TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/common/mis_com_mafr.fpk"
     TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/snd_ene_af.fpk"--EnemyType.TYPE_PF, CpType.TYPE_AFRIKAANS
-    TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/quest_block.fpk"
+    TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/subs_boot_ene_af.fpk"-- f30020_subtitles -> <lang>Voice/<lang>Text/ene_common_af.subp,_str.subp
+    TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/ih/quest_block.fpk"--DEBUGNOW test if solder locators work here or if they need to be after Soldier2GameObject in _mission pack
     TppPackList.AddMissionPack"/Assets/tpp/pack/mission2/free/fafc0/fafc0_mission.fpk"
   end,
   fovaSetupFunc=function(locationName,missionId)
