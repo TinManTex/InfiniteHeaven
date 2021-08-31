ce9bfb6d65918acc857a52c91e718cf105fd468c
TinManTex <tinman.squad@gmail.com>
2021-08-31 16:13:04 +1200

    Create ActionIconType.lua

---
+++ tpp/mod-gamedir/modules/ActionIconType.lua
@@ -0,0 +1,16 @@
+--ActionIconType.lua
+--See ActionIconEx
+local this={
+    ACTION = 0,
+    RELOAD = 1,
+    FIRE = 2,
+    READY = 3,
+    STANCE = 4,
+    DIRECTIONAL_UP = 5,--rlc shows left stick with arrow pointing at it
+    BINOCULAR = 6,--rlc shows left stick
+    DASH = 7,--rlc shows right shoulder button
+    DIVE = 8,
+    ZOOM = 9,
+    --Seems to stop working after this
+}--this
+return this
