3618dbd28e1e57e5c1fc2c9cc916b7700312a39c
TinManTex <tinman.squad@gmail.com>
2023-03-20 16:36:24 +1300

    Update InfRequiresStart.lua

--- tpp/data1_dat-lua-ih/Assets/tpp/script/ih/InfRequiresStart.lua
+++ tpp/data1_dat-lua-ih/Assets/tpp/script/ih/InfRequiresStart.lua
@@ -7,7 +7,7 @@ local this={}
 InfCore.LogFlow"Load InfRequiresStart - start of TppDefine requires-list"
 InfCore.LogFlow"requires list: (this is just documentation, not indicating point of executution)"
 InfCore.LogFlow"if you want point of execution/load for the requires files then look in IHHook at lua_loadHook"
---tex Note: this wont catch InfMain at end since that's pushed into _requiresList instead and only done after Tpp .requires loading anyhoo (see InfInitMain)
+--tex Note: this wont catch InfMain at end since that's pushed into _requireList instead and only done after Tpp .requires loading anyhoo (see InfInitMain)
 for i,path in ipairs(Tpp.requires) do
   InfCore.LogFlow(path)
 end
