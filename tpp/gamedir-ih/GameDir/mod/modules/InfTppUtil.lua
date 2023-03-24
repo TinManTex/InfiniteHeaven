3ae3f2aead3e60ad9f2c070b671b31d1c5ec0181
TinManTex <tinman.squad@gmail.com>
2023-03-24 15:49:51 +1300

    Warp

--- tpp/gamedir-ih/GameDir/mod/modules/InfTppUtil.lua
+++ tpp/gamedir-ih/GameDir/mod/modules/InfTppUtil.lua
@@ -94,6 +94,29 @@ function this.RunOnAllObjects(objectType,instanceCount,RunFunc)
     end
   end
 end
+--tex general warp function for gameobjects that have warp/position commands
+--gameObjectId: object that you want to warp to pos, 0 for player
+--pos: {x,y,z,rotY}
+--rotY: if nil will try pos[4] 
+function this.Warp(gameObjectId,pos,rotY)
+  rotY=rotY or pos[4] or 0
+  
+  local typeIndex=GameObject.GetTypeIndex(gameObjectId)   
+  if typeIndex==TppGameObject.GAME_OBJECT_TYPE_PLAYER2 then
+    --tex TppPlayer.Warp only allows #pos==3 (so no tacked on rotY)
+    rotY=foxmath.NormalizeRadian(foxmath.DegreeToRadian(rotY))--tex really?
+    local command={id="WarpAndWaitBlock",pos=pos,rotY=rotY}
+    GameObject.SendCommand(gameObjectId,command)
+  elseif typeIndex==TppGameObject.GAME_OBJECT_TYPE_VEHICLE then
+    pos=TppMath.Vector3toTable(pos)
+    GameObject.SendCommand(gameObjectId,{id="SetPosition",position=pos,rotY=rotY})
+  elseif typeIndex==TppGameObject.GAME_OBJECT_TYPE_WALKERGEAR2 or TppGameObject.GAME_OBJECT_TYPE_COMMON_WALKERGEAR2 then--tex buddy walker or enemy walker
+    GameObject.SendCommand(gameObjectId,{id="SetPosition",pos=pos,rotY=rotY})
+  else
+    local typeName=InfLookup.TppGameObject.typeIndex[typeIndex]
+    InfCore.Log("WARNING: InfTppUtil.Warp: does not support gameObject type:"..tostring(typeName),true)
+  end--if typeIndex
+end--Warp
 
 function this.IsDemoPlaying()
   return DemoDaemon.IsDemoPaused() or DemoDaemon.IsDemoPlaying() or DemoDaemon.GetPlayingDemoId()
