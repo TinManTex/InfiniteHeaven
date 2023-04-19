acbe1ea93bad6a7cacd2daf54145785944067f76
TinManTex <tinman.squad@gmail.com>
2023-04-19 21:30:43 +1200

    WriteBaseGameVscodeHint

--- tpp/gamedir-ih/GameDir/mod/modules/InfDebug.lua
+++ tpp/gamedir-ih/GameDir/mod/modules/InfDebug.lua
@@ -3,15 +3,22 @@
 local this={}
 
 function this.PostAllModulesLoad()
-  if Ivars.debug_WriteLoadedModulesHint:Is(1)then
-    this.WriteLoadedModulesHint()
+  if Ivars.debug_WriteVscodeHint:Is(1)then
+    this.WriteVscodeHint()
   end
 end--PostAllModulesLoad
 
+function this.GetRequireLine(filePath)
+  local moduleName=InfCore.GetModuleName(filePath)
+  local fileNameWithoutExt=string.sub(filePath,1,-string.len(".lua")-1)--tex strip ext
+  local line=moduleName..[[=require"]]..fileNameWithoutExt..[["]]
+  return line
+end--GetRequireLine
+
 --tex TODO: really need to tag IH files so IH/other mods can have their own hint files
 --OUT:FILE: GameDir/mod/vscode_hint-mod.lua
-function this.WriteLoadedModulesHint()
-  InfCore.LogFlow("WriteLoadedModulesHint")
+function this.WriteLoadedModulesVscodeHint()
+  InfCore.LogFlow("WriteLoadedModulesVscodeHint")
 
   InfCore.PrintInspect(InfCore.loadedModules,"InfCore.loadedModules")
   InfCore.PrintInspect(Tpp.requires,"Tpp.requires")
@@ -25,7 +32,7 @@ function this.WriteLoadedModulesHint()
   local fileName="vscode_hint-mod.lua"
   local hintLines={
     "--"..fileName,
-    "--GENERATED: by WriteLoadedModulesHint from InfCore.loadedModules",
+    "--GENERATED: by Debug menu > WriteVscodeHint using InfCore.loadedModules",
     "--module/file hints for vscode lua language server extension",
     "--ASSUMPTION: file paths are somewhere the lua language servers Settings > Lua Runtime: Path can find them",
     "--which by default is anything downstream of the workspace folder", 
@@ -35,9 +42,7 @@ function this.WriteLoadedModulesHint()
   table.insert(hintLines,"--non base game LoadLibrary")
   for fileName,loadInfo in pairs(InfCore.loadedModules.LoadLibrary)do
     if not manifest_data_dat.fileList[fileName] and not manifest_data_dat.fileList["/"..fileName] then--tex 'Tpp/' paths dont have leading slash      
-      local moduleName=InfCore.GetModuleName(fileName)
-      local fileNameWithoutExt=string.sub(fileName,1,-string.len(".lua")-1)--tex strip ext
-      local line=moduleName..[[=require"]]..fileNameWithoutExt..[["]]
+      local line=this.GetRequireLine(fileName)
       table.insert(hintLines,line)
     end
   end--for loadedModules.LoadExternalModule
@@ -46,9 +51,7 @@ function this.WriteLoadedModulesHint()
   table.insert(hintLines,"--Tpp.requires")
   for i,fileName in ipairs(Tpp.requires)do
     if not manifest_data_dat.fileList[fileName]then
-      local moduleName=InfCore.GetModuleName(fileName)
-      local fileNameWithoutExt=string.sub(fileName,1,-string.len(".lua")-1)--tex strip ext
-      local line=moduleName..[[=require"]]..fileNameWithoutExt..[["]]
+      local line=this.GetRequireLine(fileName)
       table.insert(hintLines,line)
     end
   end--for Tpp.requires
@@ -70,10 +73,52 @@ function this.WriteLoadedModulesHint()
   local savePath=InfCore.paths.mod..fileName
   InfCore.Log("Writing "..savePath)
   InfCore.WriteLines(savePath,hintLines)
-end--WriteLoadedModulesHint
+end--WriteLoadedModulesVscodeHint
+
+--tex doesn't really need to be written each time, but on the off chance theres a game updated (and manifest luas updated to reflect changes)
+function this.WriteBaseGameVscodeHint()
+  if manifest_data_dat==nil then
+    InfCore.Log("ERROR: WriteBaseGameVscodeHint: manifest_data_dat module not loaded")
+    return
+  end
+
+  local fileName="vscode_hint-base_game.lua"
+  local hintLines={
+    "--"..fileName,
+    "--GENERATED: by Debug menu > WriteVscodeHint using manifest_data_dat, manifest_fpk_combined",
+    "--hint file for vscode lua language server extension by sumneko",
+    "--this is not acually used in actual IH mod, the vscode lua extension needs a bit of help figuring out what modules are loaded through the games (and IHs) convoluted loading process",
+    "--Assumes the files are somewhere downstream of workspace folder, ",
+    "--you can hover mouse over path and see if the popup finds the actual file path",
+    "--else would have to add to vscode Settings > Lua > Runtime: Path",
+    "--downside of not specifiying actual subfolder (ex data1_dat-lua/Assets/bleh) means it will pick up every file of the same name but in diffent folders",
+    "--(in the case of IH this might be a base file in data1_dat-lua and externally loaded one I'm kicking around in gamedir-ih/GameDir/mod)",
+    "--and I'm not sure how it actually resolves in that case",
+
+  }
+  table.insert(hintLines,"--data1_dat-lua")
+  for i,filePath in ipairs(manifest_data_dat.fileList)do
+    local line=this.GetRequireLine(filePath)
+    table.insert(hintLines,line)
+  end--for fileList
+  table.insert(hintLines,"--fpk_combined-lua")
+  for i,filePath in ipairs(manifest_fpkd_combined.fileList)do
+    local line=this.GetRequireLine(filePath)
+    table.insert(hintLines,line)
+  end--for fileList
+
+  local savePath=InfCore.paths.mod..fileName
+  InfCore.Log("Writing "..savePath)
+  InfCore.WriteLines(savePath,hintLines)
+end--WriteBaseGameVscodeHint
+
+function this.WriteVscodeHint()
+  this.WriteBaseGameVscodeHint()
+  this.WriteLoadedModulesVscodeHint()
+end--WriteVscodeHint
 
 --tex in debugMenu
-this.debug_WriteLoadedModulesHint={
+this.debug_WriteVscodeHint={
   inMission=true,
   save=IvarProc.CATEGORY_EXTERNAL,
   range=Ivars.switchRange,
@@ -81,7 +126,7 @@ this.debug_WriteLoadedModulesHint={
 }
 
 this.registerIvars={
-  "debug_WriteLoadedModulesHint",
+  "debug_WriteVscodeHint",
 }
 
 this.registerMenus={
@@ -109,8 +154,8 @@ this.debugMenu={
     "Ivars.telopMode",--tex TODO move, odd one out, mission/presentation?
     "Ivars.manualMissionCode",
     "Ivars.manualSequence",
-    "Ivars.debug_WriteLoadedModulesHint",
-    "InfDebug.WriteLoadedModulesHint",
+    "Ivars.debug_WriteVscodeHint",
+    "InfDebug.WriteVscodeHint",
   }
 }--debugMenu
 
@@ -164,8 +209,8 @@ this.langStrings={
   },
   help={
     eng={
-      debug_WriteLoadedModulesHint="Runs WriteLoadedModulesHint on PostAllModulesLoad",
-      writeLoadedModulesHint="Writes GameDir/mod/vscode_hint-mod.lua, writes a hint file for vscode lua language server extension of mod lua files loaded.",
+      debug_WriteVscodeHint="Runs WriteLoadedModulesHint on PostAllModulesLoad",
+      writeVscodeHint="Writes GameDir/mod/vscode_hint-mod.lua,vscode_hint-base_game.lua , writes a hint files for vscode lua language server extension of mod lua files loaded.",
     },
   },
 }--langStrings
