efd3537d40cdd540e153001befa2e708835a313c
TinManTex <tinman.squad@gmail.com>
2023-05-29 08:05:13 +1200

    fix non ihhook trying to InfCore.Log before its actually operational (due to OnModuleLoad change)

--- tpp/data1_dat-lua-ih/Assets/tpp/script/ih/InfInit.lua
+++ tpp/data1_dat-lua-ih/Assets/tpp/script/ih/InfInit.lua
@@ -6,6 +6,21 @@
 -- see also InfInitMain
 local this={}
 
+--tex a lot of chicken egging here to load InfCore externally
+local function CmdEcho(message)
+  --tex cmd /k will keep cmd open. I haven't had much luck getting pause to work outside of a batch file
+  --problem with /k is mgsv wont start again unless its closed.
+  local strCmd=[[cmd.exe /k echo ]]..message
+  os.execute(strCmd)
+end
+local function LogError(message)
+  if IHH then
+    IHH.Log(message)
+  else
+    CmdEcho(message)
+  end
+end
+
 if IHH then
   IHH.Log("InfInit.lua")
   IHH.Init()
@@ -14,41 +29,68 @@ end
 local modSubPath="mod"
 local corePath="/Assets/tpp/script/ih/InfCore"
 
-local exists=true
+--tex try and load InfCore externally
+--currently InfCore is only external on dev build
+--IH release has it internal
+
+--tex using require at this point relies on default package.path (
+--or LUA_PATH with ;; which will insert default path, but that's a lot more chicken egging to parse to check, just to do it again in InfCore
+-- local luaPathEnv=os.getenv("LUA_PATH")
+-- if luaPathEnv then
+
+-- end
+
+local externalExists=true
+local extSuccess=false
+local externalRet=nil
 if IHH then
-  exists=IHH.FileExists("./"..modSubPath..corePath..".lua")--WORKAROUND: now that IHHook is catching errors we want to skip just blindly require ing
+  externalExists=IHH.FileExists("./"..modSubPath..corePath..".lua")--WORKAROUND: now that IHHook is catching errors we want to skip just blindly require ing
 end
-if exists then
+if externalExists then
   --tex try loading external first. messy, but it's chicken and egg, once InfCore is up and running it has tidier functions that could do this
-  local sucess,ret=pcall(require,modSubPath..corePath)--GOTCHA modPath. relying on default package.paths having game dir DEBUGNOW if IHH then could use gamepath
-  if sucess then
-    InfCore=ret
-    InfCore.Log("Loaded InfCore externally")
+  extSuccess,externalRet=pcall(require,modSubPath..corePath)--GOTCHA modPath. relying on default package.paths having game dir DEBUGNOW if IHH then could use gamepath
+  if extSuccess then
+    InfCore=externalRet
+    --tex InfCore.Log still not up till OnModuleLoad
+    if IHH then
+      IHH.Log("Loaded InfCore externally")
+    end
   else
-    exists=false
+    externalExists=false
   end
 end
 
-if not exists then
-  --TODO I don't think there's really any option here to log if InfCore fails and IHHook isn't installed.
-  --also, this is only really an error if -v- InfCore doesn't exist internally
-  --does loadlibrary raise errors that we can catch?
+if not extSuccess then
+  --TODO: does loadlibrary raise errors that we can catch?
   if IHH then
     --IHH.Log(5,ret)--level_critical
   end
   Script.LoadLibrary(corePath..".lua")
-  InfCore.Log("Loaded InfCore internally")
+  if IHH then
+    IHH.Log("Loaded InfCore internally")
+  end
 end
 
 if not InfCore then
-  if IHH then
-    IHH.Log("ERROR: InfCore not loaded")
+  if externalExists and not extSuccess then
+    --tex a bit twisty due to the above. basically InfCore failed to load external or internal,
+    --if theres an external then its a dev build, so we can log the earlier fail
+    --release build doesnt have external, so its externalRet will just be complaining about it being missing
+    --TODO: so we arent actually catching the case for release where internal InfCore load fails
+    --but I'm not sure we have enough info to figure that out unless we have some build step flag what's expected (there being an external or not)
+    LogError("ERROR: require "..tostring(modSubPath..corePath).." did not succeed: "..tostring(externalRet))
+  else
+    LogError("ERROR: InfCore did not load")
   end
 else
-  InfCore.OnModuleLoad()
+  local success,ret=pcall(InfCore.OnModuleLoad)
+  if not success then
+    LogError("ERROR: InfCore.OnModuleLoad: "..tostring(ret))
+  end
 end
 
 if InfCore and not InfCore.modDirFail then
+  --tex non IHH log should now be available
   InfCore.LoadExternalModule"InfInspect"
   InfCore.LoadExternalModule"InfUtil"
   InfCore.LoadExternalModule"InfHooks"
