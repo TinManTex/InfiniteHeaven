0591fa01f33db799abdc17dd6c89d5a0da24cc9b
TinManTex <tinman.squad@gmail.com>
2023-06-10 16:43:11 +1200

    split info to file

--- tpp/gamedir-ih/GameDir/mod/modules/InfBossTppVolgin2.lua
+++ tpp/gamedir-ih/GameDir/mod/modules/InfBossTppVolgin2.lua
@@ -60,47 +60,8 @@ this.gameIdToNameIndex={}--InitEvent
 this.appearedTime=0
 this.appearInWaterEscapeTime=10
 
---addons>
-this.names={
-  "VOLGIN",
-}
-this.infos={
-  VOLGIN={
-    packages={
-      --tex mantis appears when volgin downed
-      --you can simply not load the mantis packs and he wont appear, volgin will still work 
-      --(VERIFY, I've had some weird back and forths with this during breaking apart and reconstructing volgin stuff, stuff loading when they in theory coulnt)
-      --TODO: document any other behavior during vanilla
-      "/Assets/tpp/pack/mission2/common/mis_com_mantis.fpk",
-      --TODO: combine maybe
-      --but no tpp_mantis2.fox2
-      --TppMantis2GameObjectLocator
-      --mantis mog,mtar
-      "/Assets/tpp/pack/boss/ih/TppMantis2/TppMantis2_gameobject.fpk",--tex not really its own boss (or is it the one true boss of tpp), but keeping naming consistanct
-      "/Assets/tpp/pack/boss/ih/TppMantis2/TppMantis2_locator.fpk",
-
-      --tex just parts, no gameobject
-      "/Assets/tpp/pack/mission2/common/mis_com_volgin.fpk",
-      --based on s10110_npc_sp.fox2
-      --Gameobject
-      --TppVfxFileLoader with referenced files (that aren't already in mis_com_volgin)
-      --Volgin2_layers.mog
-      --Volgin2_layers.mtar
-      "/Assets/tpp/pack/boss/ih/TppVolgin2/TppVolgin2_gameobject.fpk",
-    },
-    objectNames={
-      "vol_factory_0000",
-    },
-    eventParams={
-      spawnRadius=10,--ivar
-      --zombifies=true,--TODO: set false and test the boss objects zombifying ability
-      fultonable=true,
-      faction="SKULL",
-      weather="CLOUDY",--see InfBossEvent weatherTypes
-    },
-  },--VOLGIN
-}--infos
---<
+this.names={}
+this.infos={}
 
 this.enableSubTypeIvarNames={}
 
